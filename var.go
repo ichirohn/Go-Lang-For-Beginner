0bfda31a9b63aa6f72c75ad16ee82aa2ce4a5c6a
ichirohn <32905698+ichirohn@users.noreply.github.com>
2022-10-25 06:37:13 +0700

    Add files via upload

---
+++ var.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Ichiro Muhammad"
+	fmt.Println(name)
+
+	name = "Muhammad Ichiro"
+	fmt.Println(name)
+
+	var friendName = "Yayang"
+	fmt.Println(friendName)
+
+	var age = 24
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Ichiro"
+		lastName  = "Muhammad"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
