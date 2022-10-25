5ecf268760736824a2599b0632da3842e662d086
ichirohn <32905698+ichirohn@users.noreply.github.com>
2022-10-25 10:04:10 +0700

    Add files via upload

---
+++ constant.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName string = "Ichiro"
+		lastName         = "Muhammad"
+		value            = 100
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+
+}
