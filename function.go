ec074abec51b0831089deaa0046e435611c86a30
ichirohn <32905698+ichirohn@users.noreply.github.com>
2022-11-24 16:34:39 +0700

    Add files via upload

---
+++ function.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func sayHi() {
+	fmt.Println("Hello World")
+
+}
+
+func main() {
+	sayHi()
+	sayHi()
+	sayHi()
+	sayHi()
+}
+
+//menggunakan perulangan
+//func main() {
+//for i := 0; i < 10; i++ {
+//sayHi()
+
+//}
+
+//}
